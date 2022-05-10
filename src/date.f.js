1668036f424c956738082a6e6177eaeffdace5af
Joshua H. Faulkenberry <j@joshuafaulkenberry.com>
2022-05-09 17:36:19 -0700

    Update

--- src/date.f.js
+++ src/date.f.js
@@ -1,7 +1,7 @@
 /*!
  * F is for Format, WHAT THE diff, & Friends v2.3
  *
- * Kopimi 2021 Joshua Faulkenberry
+ * Kopimi 2022 Joshua Faulkenberry
  * Unlicensed under The Unlicense
  * http://unlicense.org/
  */
